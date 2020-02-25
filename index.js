4167251fd2ca4979614bb29c07a151af22016856
Andrew Bjork <bjorkandrew1@gmail.com>
2020-02-24 22:04:48 -0600

    updating content

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function ProfileGen(userInfo) {
+/*module.exports = function ProfileGen(userInfo) {
 
     this.name = userInfo.name;
 
@@ -21,20 +21,20 @@ module.exports = function ProfileGen(userInfo) {
     </html>`
 
 }
+*/
 
 var pdf = require('pdf-creator-node');
 var fs = require('fs');
 
-var html = fs.readFileSync('index.html','utf8');
+var html = fs.readFileSync('index.html', 'utf8');
 
 var inquirer = require("inquirer");
 
 var axios = require('axios');
 
-const https = require('https');
 
 
-const ProfGen = require('./generator');
+
 
 
 
@@ -52,18 +52,18 @@ this.userDetails = {};
 
 
 inquirer
-  .prompt([
+    .prompt([
 
-    {
-      type: "input",
-      message: "What is your username?",
-      name: "username"
-      },
-    {
-      type: "input",
-      message: "What is your favorite color?",
-      name: "color"
-    }
+        {
+            type: "input",
+            message: "What is your username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "What is your favorite color?",
+            name: "color"
+        }
     ])
 
 
@@ -71,60 +71,124 @@ inquirer
 
     .then(answers => {
 
-        console.log(https.get('https://api.github.com/users/' + answers.username.name))
 
 
-        var options = {
-            format: "A3",
-            orientation: "portrait",
-            border: "10mm",
-            header: {
-                height: "45mm",
-                contents: '<div style="text-align: center; background-color:' + answers.color + '">Author: Andrew Bjork</div>'
-            },
 
-            "footer": {
-                "height": "28mm",
-                "contents": {
-                first: 'Cover page',
-                2: 'Second page', // Any page number is working. 1-based index
-                default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
-                last: 'Last Page'
-            }
-        }
-        };
+        axios.get(`https://api.github.com/users/${answers.username}`)
+            .then(function (response) {
+                
+                let avatar_url = response.data.avatar_url;
+                
+                let url = response.data.url
+                let html_url = response.html_url;
+                
+                let blog = response.data.blog;
+                let location = response.data.location;
+                let email = response.data.email;
+                
+                let bio = response.data.bio;
+                let public_repos = response.data.public_repos;
+                let public_gists = response.data.public_gists;
+                let followers = response.data.followers;
+                let following = response.data.following;
+                
+                console.log(response)
+                html = `<!DOCTYPE html>
+                <html>
+                    <head>
+                        <mate charest="utf-8" />
+                        <title>Hello world!</title>
+                    </head>
+                    <body>
+                        <h1>User List</h1>
+                        <ul>
+                            
+                            <li>Name: ${answers.username}</li>
+                            <li>Avatar_url :<img src = ${avatar_url} ></li>
+                            <li>Avatar_url : ${url}</li>
+                            <li>Avatar_url : ${html_url}</li>
+                            <li>Avatar_url : ${blog}</li>
+                            <li>Avatar_url : ${location}</li>
+                            <li>Avatar_url : ${email}</li>
+                            <li>Avatar_url : ${bio}</li>
+                            <li>Avatar_url : ${public_repos}</li>
+                            <li>Avatar_url : ${public_gists}</li>
+                            <li>Avatar_url : ${followers}</li>
+                            <li>Avatar_url : ${following}</li>
+                            <li>Age: 0</li>
+                            
+                      
+                        </ul>
+                    </body>
+                </html>`
+
+
+                var document = {
+                    html: html,
+                    data: {
+                        answers: answers
+                    },
+
+
+                    path: "./output.pdf"
+                };
+
+                var options = {
+                    format: "A3",
+                    orientation: "portrait",
+                    border: "10mm",
+                    header: {
+                        height: "45mm",
+                        contents: '<div style="text-align: center; background-color:' + answers.color + '">Author: Andrew Bjork</div>'
+                    },
         
-        var users = [
-            {
-                name: answers.username,
-                age: https.get('https://api.github.com/users/' + answers.username.name)
-            }
-        ]
+                    "footer": {
+                        "height": "28mm",
+                        "contents": {
+                            first: 'Cover page',
+                            2: 'Second page', // Any page number is working. 1-based index
+                            default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+                            last: 'Last Page'
+                        }
+                    }
+                };
+
+                pdf.create(document, options)
+                    .then(res => {
+                        console.log(res)
+                    })
+                    .catch(error => {
+                        console.error(error)
+                    });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
 
         
-        var document = {
-            html: html,
 
-            
-            data: {
-                users: users,
-            },
-            path: "./output.pdf"
-        };
 
-           
+
+        /*
+                var document = {
+                    html: html,
+        
+        
+                    data: {
+                        users: users,
+                    },
+                    path: "./output.pdf"
+                };
+                */
+
+
 
         // Know this works
-        
-        
-        pdf.create(document, options)
-            .then(res => {
-                console.log(res)
-            })
-            .catch(error => {
-                console.error(error)
-            });
-        
+
+
+
+
 
 
     });
@@ -136,7 +200,7 @@ inquirer
 
 
 
-    
+
 
 
 /*
@@ -144,7 +208,7 @@ inquirer
 var pdf = require('html-pdf');
 var html = fs.readFileSync('./test/businesscard.html', 'utf8');
 var options = { format: 'Letter' };
- 
+
 pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
@@ -156,11 +220,11 @@ var pdf = require('html-pdf');
 pdf.create(html).toFile([filepath, ]function(err, res){
   console.log(res.filename);
 });
- 
+
 pdf.create(html).toStream(function(err, stream){
   stream.pipe(fs.createWriteStream('./foo.pdf'));
 });
- 
+
 pdf.create(html).toBuffer(function(err, buffer){
   console.log('This is a buffer:', Buffer.isBuffer(buffer));
 });
@@ -171,7 +235,7 @@ pdf.create(html).toBuffer(function(err, buffer){
 
 
 /*
-var doc = new jsPDF();          
+var doc = new jsPDF();
 var elementHandler = {
   '#ignorePDF': function (element, renderer) {
     return true;
@@ -216,10 +280,10 @@ github.authenticate({
 /* gets all repos as an array
  */
 
- /*
+/*
 
 github.repos.getAll({}, function(err, list) {
-    console.log( list );
+   console.log( list );
 });
 
 
@@ -230,20 +294,20 @@ var inquirer = require("inquirer");
 //Prompted for username and favorite color
 
 inquirer
-  .prompt([
+ .prompt([
 
-    {
-        type: "input",
-        message: "What is your username?",
-        name: "username"
-      },
-    {
-      type: "input",
-      message: "What is your favorite color?",
-      name: "color"
-    }
-    ]);
+   {
+       type: "input",
+       message: "What is your username?",
+       name: "username"
+     },
+   {
+     type: "input",
+     message: "What is your favorite color?",
+     name: "color"
+   }
+   ]);
 
-    //PDF profile is generated
+   //PDF profile is generated
 
 */
